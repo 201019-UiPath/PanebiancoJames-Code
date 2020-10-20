288cd5e732084f434f2f76b8d8d6947430cdf2fe
James Panebianco <daxterscool41@gmail.com>
2020-10-20 16:53:48 -0400

    Modern class and Arrays

---
+++ HerosApp/HerosLib/Hero.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace HerosLib
+{
+    #region old way of creating class members
+
+    /*
+    public class Hero
+    {
+        // default mod of members of a class is private
+        /*int id; // value type => System.Int32
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id=value;
+            }
+        }
+
+        public int Id{get;set;} // autonomous property
+        public string name; // reference type
+
+        // Constructor
+        public Hero()
+        {
+            //id=1;
+            name="Bombasto";
+        }
+        
+        // snippect ctor + double <tab> key
+        public Hero(int id, string name)
+        {
+            //this.id = id;
+            Id = id;
+            this.name = name;
+        }
+    }
+    */
+    #endregion
+    #region Modern way of creating class member
+    
+    public class Hero
+    {
+            public int Id { get; set; }
+            public string Name { get; set; }
+
+            public string[] superPowers = new string[10]; // 1-D
+
+            //jagged array
+            public int[][] ja = new int[3][];
+            
+
+    }
+    #endregion
+}
