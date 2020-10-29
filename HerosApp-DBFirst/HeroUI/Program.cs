a896925a51f593a3149cd8343ee7ed54b98719b1
James Panebianco <daxterscool41@gmail.com>
2020-10-29 12:23:27 -0400

    DB First

---
+++ HerosApp-DBFirst/HeroUI/Program.cs
@@ -0,0 +1,14 @@
+﻿using HerosDB.Entities;
+using HerosDB;
+namespace HeroUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMenu main = new MainMenu(new HeroContext(), new DBMapper());
+            main.start();
+        }
+
+    }
+}
