eba2cc036ca3fcc9f8320ac3d5ba8f5d9cd08024
James Panebianco <daxterscool41@gmail.com>
2020-10-29 14:48:42 -0400

    ADO

--- HerosApp-DBFirst/HeroUI/Program.cs
+++ HerosApp-DBFirst/HeroUI/Program.cs
@@ -1,13 +1,18 @@
 ﻿using HerosDB.Entities;
 using HerosDB;
+using HerosDbWithADO;
 namespace HeroUI
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Data db = new Data();
+            db.GetSuperPersonConnected();
+            /*
             IMenu main = new MainMenu(new HeroContext(), new DBMapper());
             main.start();
+            */
         }
 
     }
---
+++ HerosApp-DBFirst/HerosDbWithADO/Data.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Npgsql;
+
+namespace HerosDbWithADO
+{
+    public class Data
+    {
+        // path to database
+        string connectionStr = "Host=lallah.db.elephantsql.com;Port=5432;Database=nftaixly;Username=nftaixly;Password=KWmPgfSJIVICyNybdRN0X-zWWQxngr8L";
+        
+        // create a conneciton
+        NpgsqlConnection connection;
+
+        // fire the query
+        NpgsqlCommand command;
+
+        // get the results
+        NpgsqlDataReader reader;
+
+
+        string query = "select * from Superpeople";
+        public void GetSuperPersonConnected()
+        {
+            try{
+                connection = new NpgsqlConnection(connectionStr);
+                connection.Open();
+                //command = new NpgsqlCommand(connection:connection, cmdText:query); // using named parameters
+                command = new NpgsqlCommand(query,connection);
+                // return the results of query
+                reader = command.ExecuteReader();
+                // ExecuteReader reads value, used with select queries
+                // ExecuteNonQuary used with Insert, delete and update
+                while(reader.Read())
+                {
+                    System.Console.WriteLine($"{reader[0]} {reader[1]}");
+                }
+            }
+            catch(Exception ex) {
+                Console.WriteLine(ex.Message);
+
+                // Log the exception (Serilogg)
+            }
+            finally {
+                connection.Close();
+            }
+        }
+    }
+}
